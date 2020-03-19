72aa3ba6e754207cd5bba8a4d73483b7ea89f80d
Sacha  TORRES <vramius1017@users.noreply.github.com>
2020-03-19 18:43:16 +0100

    commit - m "modes inm desease  pub"

--- inca/models.py
+++ inca/models.py
@@ -12,19 +12,61 @@ db = CQLAlchemy(app)
 
 # famille de colonnes  INM 
 class Scats_by_cat(db.Model):
-    idc = db.columns.Integer()
+    idc = db.columns.Integer() # cluster Key
     catcode = db.columns.Integer(primary_key=True)
     catname = db.columns.Integer(primary_key=True)
     scatcode = db.columns.Integer(primary_key=True)
     scatname = db.columns.Integer(primary_key=True)
     
 class Inms_by_scat(db.Model):
-    idi = db.columns.Integer()
+    idi = db.columns.Integer() # Cluster Key
     catcode = db.columns.Integer(primary_key=True)
     catname = db.columns.Integer(primary_key=True)
     scatcode = db.columns.Integer(primary_key=True)
     scatname = db.columns.Integer(primary_key=True)
-    charac = db.columns.Text(default="none")
+    charac = db.columns.Text(primary_key=True,default="none")
+
+
+# famille de colonnes Desease
+class Desease(db.Model):
+    pass
+
+class Cancer(db.Model):
+    pass
+
+class Diabetes(db.Model):
+    pass
+
+class Synom(db.Model):
+    pass
+
+class Localisation(db.Model):
+    pass
+
+# famille de colonnes publication 
+
+class Meta(db.Model):
+    pass
+
+class Sok(db.Model):
+    pass
+
+class Sko(db.Model):
+    pass
+
+class samb(db.Model):
+    pass
+
+class Pub(db.Model):
+    pass
+
+class Nopub(db.Model):
+    pass
+
+# famille de colonnes admin_app
+
+    
+
     
         
     
