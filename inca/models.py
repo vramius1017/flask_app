8f730dda16a8ab29b4ea10a5e4edbcae3e1de4d1
Sacha  TORRES <vramius1017@users.noreply.github.com>
2020-04-07 18:25:31 +0200

    functions flask

--- inca/models.py
+++ inca/models.py
@@ -1,77 +1,99 @@
 # coding utf8
 import uuid
 from flask import Flask
-from flask.ext.cqlalchemy import CQLAlchemy
-
+from flask_cassandra import CassandraCluster
+from cassandra.cqlengine.columns import columns
+from cassandra.cqlengine.models import Model
+from cassandra.cqlengine.management import sync_table
 #declaration du cluser Cassandra local URL et nom du keyspace inca_test
 app = Flask(__name__)
+cassandra = CassandraCluster()
+
 app.config['CASSANDRA_HOSTS']=['127.0.0.1:9042']
 app.config['CASSANDRA_KEYSPACE'] = "inca_test"
-db = CQLAlchemy(app)
 
-
-# famille de colonnes  INM 
-class Scats_by_cat(db.Model):
-    idc = db.columns.Integer() # cluster Key
-    catcode = db.columns.Integer(primary_key=True)
-    catname = db.columns.Integer(primary_key=True)
-    scatcode = db.columns.Integer(primary_key=True)
-    scatname = db.columns.Integer(primary_key=True)
+# selection des apps 
+class links(Models):
+    __table_name__ = 'links'
+    idap = columns.Integer(primary_key=True)
+    name = columns.Text()
+    url = columns.Text()
     
-class Inms_by_scat(db.Model):
-    idi = db.columns.Integer() # Cluster Key
-    catcode = db.columns.Integer(primary_key=True)
-    catname = db.columns.Integer(primary_key=True)
-    scatcode = db.columns.Integer(primary_key=True)
-    scatname = db.columns.Integer(primary_key=True)
-    charac = db.columns.Text(primary_key=True,default="none")
+# famille de colonnes  INM 
+
+class Scats_by_cat(Model):
+    __table_name__ = 'scats_by_cat'
+    
+    
+class Inms_by_scat(Model):
+    id_inm = columns.Integer(primary_key=True,clustering_order="ASC") # Cluster Key
+    catcode = columns.Integer(primary_key=True)
+    catname = columns.Integer(primary_key=True)
+    scatcode = columns.Integer(primary_key=True)
+    scatname = columns.Integer(primary_key=True)
+    charac = columns.Text(primary_key=True,default="none")
 
 
 # famille de colonnes Desease
-class Desease(db.Model):
+class Desease(Model):
     pass
 
-class Cancer(db.Model):
+class Cancer(desease):
     pass
 
-class Diabetes(db.Model):
+class Diabetes(desease):
     pass
 
-class Synom(db.Model):
+class Synom(Model):
     pass
 
-class Localisation(db.Model):
+class Localisation(Model):
     pass
 
 # famille de colonnes publication 
 
-class Meta(db.Model):
+class Meta(Model):
     pass
 
-class Sok(db.Model):
+class Sok(Model):
     pass
 
-class Sko(db.Model):
+class Sko(Model):
     pass
 
-class samb(db.Model):
+class samb(Model):
     pass
 
-class Pub(db.Model):
+class Pub(Model):
     pass
 
-class Nopub(db.Model):
+class Nopub(Model):
     pass
 
 # famille de colonnes admin_app
 
     
+class User(Model):
+    pass
 
+class colab(Model):
+    id_cob = columns.integer(primary_key=True)
+    name_cob = columns.Text(required=True)
+    pre_cob = columns.Text(required=True)
+    mail_cob = columns.text(primary_key=True,clustering_order="ASC") 
+    keycol = columns.Text(required=True)
     
         
-    
+def ConectCass():
+     session = cassandra.connect()
+     session.set_keyspace("inca_test") 
 
-    
+
+def SyncDb(cf):
+     return sync_table(cf)      
+
+if __name__ == '__main__':
+    app.run()  
         
 
     
--- inca/views.py
+++ inca/views.py
@@ -1,18 +1,42 @@
 from flask import Flask,render_template,request,url_for
 import json
+from cassandra.cluster import Cluster
 
 app = Flask(__name__)
 app.config.from_object('config')
-
+cluster = Cluster(['127.0.0.1','9042'])
+session = cluster.connect()
+session.set_keyspace("inca_test")
 
 @app.route("/")
 def index():
-    links=["www.inmcancer.fr","www.niri.fr","www.nishare.fr"]    
-    return render_template('index.html', links=links)
+    cqlink = "SELECT name,url FROM links"
+    liens = session.execute(cqlink)  
+    return render_template('index.html',liens=liens)
 
 @app.route("/selection/")
 def selection():
     return render_template('search.html')
+
+@app.route("/selection/scat")
+def sel_scat():
+    pass
+
+@app.route("/selection/cat/")
+def sel_cat():
+    cqcat = "SELECT catname FROM cat"
+    cats = session.execute(cqcat)
+    resc = request.form
+    qcat = resc['name']
+    cqscat = "select * from scats_by_cat where catname = 'Psychological health intervention'" # +str(qcat)
+    scats = session.execute(cqscat)
+    return render_template('sel_cat.html',cats=cats,scats=scats)
+
+
+
+@app.route("/selection/inm")
+def sel_inm():
+    pass
 
 @app.route("/proposition/")
 def proposition():
@@ -26,3 +50,16 @@ def validate():
 @app.route("/divers/")
 def   divers():
     return  render_template('divers.html')  
+
+
+@app.route("/404/")
+def PageError():
+    error="Page 404"
+    return render_template('404.html',error=error)
+
+
+@app.route("/travaux/")
+def PageTravaux():
+    error = "Page en travaux"
+    im = url_for('static',filename='img/tr.jpg')
+    return render_template('travaux.html',error=error,im=im)
